c268d7ce59b9f282682497d98ede79f8ee255ef4
Stfsmm <2760761541@qq.com>
2024-11-22 09:58:47 +0800

    Feat: URP project setting.

--- Assets/Scripts/GameMgr.cs
+++ Assets/Scripts/GameMgr.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameMgr : MonoBehaviour
 {
+    public static GameMgr I;
+
+    void Awake()
+    {
+        I = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
